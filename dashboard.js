fab47cdd72ccee78268822709cd12263b28a4fd4
Sohail <mr.sohail451@gmail.com>
2026-09-20 12:19:21 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,4 +1,4 @@
-const API_URL='https://script.google.com/macros/s/AKfycbzlzY7ESVKgJ_yjm2yKHorUM8szJYJdcZt0vBZBEMM7h8P_0X63RkPOPWncqx2XtZyT/exec';
+const API_URL='https://script.google.com/macros/s/AKfycbwOBIZ5LoTCMbiO09_HPM5W74H-9fQ_QtrBNlhWKXdksRxgNkG3NHeXhvcJvXCpHFr7EQ/exec';
 const ADMIN_PIN='2580'; // Must match ADMIN_PIN in your Apps Script.
 const $=id=>document.getElementById(id);
 const esc=s=>String(s??'').replace(/[&<>"']/g,c=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#039;'}[c]));
